10311d0ff4b60aeb5d69b5d317fb1fa9a7c31c66
gtalarico <gtalarico@gmail.com>
2017-10-21 21:16:20 -0400

    Added fair distribution, fixed celery?

--- heroku.sh
+++ heroku.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-celery worker -A honeybee_server.celery --loglevel=info
+celery worker -A honeybee_server.celery -Q random-tasks --concurrency=4 --loglevel=info &
 gunicorn honeybee_server:flask_app
